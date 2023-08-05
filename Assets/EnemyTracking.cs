f11c8d102e7576d4cd6848a05d9b3c142bd731b3
TSITCL\it232288 <it232288@trident.ac.jp>
2023-08-05 15:36:52 +0900

    エネミーがプレイヤーに追跡するようにしました

---
+++ Assets/EnemyTracking.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //public string targetobjectName;     //目標オブジェクト名 
+    public GameObject targetObject;    //追いかける対象
+    private float speed = 3;            //追いかけるスピード
+
+    //GameObject[] target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //targetObject = GameObject.Find(targetobjectName);
+        targetObject = GameObject.FindWithTag("Player");
+    }
+
+
+    private void FixedUpdate()//ずっと、目標オブジェクトtの方向を調べる
+    {
+        Vector3 dir = (targetObject.transform.position - this.transform.position).normalized;
+
+        //その方向へ指定した量で進む
+        float vx = dir.x * speed;
+        float vy = dir.y * speed;
+        this.transform.Translate(vx / 50, vy / 50, 0);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
